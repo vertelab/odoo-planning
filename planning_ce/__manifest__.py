21d46fa1108b70801e7022d0019813b79bd19438
jakobkrabbe <33831118+jakobkrabbe@users.noreply.github.com>
2021-04-09 13:54:18 +0200

    Create __manifest__.py

---
+++ planning_ce/__manifest__.py
@@ -0,0 +1,32 @@
+# -*- coding: utf-8 -*-
+##############################################################################
+#
+#
+#
+##############################################################################
+
+{
+    'name': 'Planning CE",
+    'version': '13.0.0.1',
+    'category': 'Project Timeline',
+    'description': """
+Using Timeline to plan the work in a team
+======================================
+
+More information:
+    """,
+    'author': 'Vertel AB',
+    'license': 'AGPL-3',
+    'website': 'http://www.vertel.se',
+    'depends': ['', ''],
+    'data': [
+        #'views/project_scrum_view.xml',
+       ],
+    #'external_dependencies': {
+        #'python' : ['bs4'],
+    #},
+    #'demo': #['demo/project_scrum_demo.xml'],
+    'installable': True,
+}
+
+# vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
