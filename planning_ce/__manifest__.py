2be4725dac08845542f55fc9058164892462aee7
jakobkrabbe <33831118+jakobkrabbe@users.noreply.github.com>
2022-08-24 16:35:46 +0200

    Update __manifest__.py
    preparing for publish at apps.odoo.com

--- planning_ce/__manifest__.py
+++ planning_ce/__manifest__.py
@@ -1,8 +1,8 @@
 # -*- coding: utf-8 -*-
 ##############################################################################
 #
-#    OpenERP, Open Source Management Solution, third party addon
-#    Copyright (C) 2021- Vertel AB (<http://vertel.se>).
+#    Odoo SA, Open Source Management Solution, third party addon
+#    Copyright (C) 2022- Vertel AB (<https://vertel.se>).
 #
 #    This program is free software: you can redistribute it and/or modify
 #    it under the terms of the GNU Affero General Public License as
@@ -15,23 +15,30 @@
 #    GNU Affero General Public License for more details.
 #
 #    You should have received a copy of the GNU Affero General Public License
-#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#    along with this program. If not, see <http://www.gnu.org/licenses/>.
 #
 ##############################################################################
 
 {
-    'name': 'Planning CE',
+    'name': 'Planning: CE',
     'version': '14.0.1.1.0',
-    'category': 'Project Timeline',
+    # Version ledger: 14.0 = Odoo version. 1 = Major. Non regressionable code. 2 = Minor. New features that are regressionable. 3 = Bug fixes
+    'summary': '',
+    'category': 'Administration',
     'description': """
 Using Timeline to plan the work in a team
 =================================================
 
 More information:
     """,
+    #'sequence': '1',
     'author': 'Vertel AB',
+    'website': 'https://vertel.se/apps/odoo-planning/planning_ce',
+    'images': ['static/description/banner.png'], # 560x280 px.
     'license': 'AGPL-3',
-    'website': 'https://www.vertel.se',
+    'contributor': '',
+    'maintainer': 'Vertel AB',
+    'repository': 'https://github.com/vertelab/odoo-planning',
     'depends': ['hr', 'project', 'sale', 'web_timeline', 'hr_timesheet', 'hr_contract'],
     'data': [
         'security/security.xml',
@@ -47,3 +54,5 @@ More information:
        ],
     'installable': True,
 }
+
+# vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
