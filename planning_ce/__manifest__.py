97deb6263c8032ef04abfa6e1e72d8a99c5826a5
jakobkrabbe <33831118+jakobkrabbe@users.noreply.github.com>
2022-09-15 11:15:14 +0200

    Update __manifest__.py

--- planning_ce/__manifest__.py
+++ planning_ce/__manifest__.py
@@ -24,7 +24,7 @@
     'version': '14.0.1.1.0',
     # Version ledger: 14.0 = Odoo version. 1 = Major. Non regressionable code. 2 = Minor. New features that are regressionable. 3 = Bug fixes
     'summary': '',
-    'category': 'Administration',
+    'category': 'Project',
     'description': """
 Using Timeline to plan the work in a team
 =================================================
