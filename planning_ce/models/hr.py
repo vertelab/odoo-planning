9cc59eab09f127aa90dfad03695f71178ba38e05
Kriss-Ellen Svensson <kriss-ellen.svensson@vertel.se>
2023-02-20 07:13:57 +0000

    [FIX]Might fix a bugg on vertel

--- planning_ce/models/hr.py
+++ planning_ce/models/hr.py
@@ -10,7 +10,7 @@ _logger = logging.getLogger(__name__)
 class Employee(models.Model):
     _inherit = "hr.employee"
 
-    default_planning_role_id = fields.Many2one('planner_ce.role', string="Planning Role")
+    # default_planning_role_id = fields.Many2one('planner_ce.role', string="Planning Role")
     planning_role_ids = fields.Many2many('planner_ce.role', string="Planning Role IDS")
 
     def action_view_planning(self):
