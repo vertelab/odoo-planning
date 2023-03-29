5e0695d5d04a6253ad2d18a481bcff11c90ecfa5
Tommy Lindell <tommy.lindell@vertel.se>
2023-03-29 09:06:46 +0000

    Planning CE: added timesheets to the comparison form view

--- planning_ce/models/ce_planner_report.py
+++ planning_ce/models/ce_planner_report.py
@@ -1,10 +1,9 @@
 # -*- coding: utf-8 -*-
 # Part of Odoo. See LICENSE file for full copyright and licensing details.
-import logging
 
 from odoo import api, fields, models, _
 from datetime import datetime, timedelta
-
+import logging
 _logger = logging.getLogger(__name__)
 
 
@@ -14,10 +13,21 @@ class PlannerCeReport(models.Model):
     project_id = fields.Many2one("project.project", "Project")
     employee_id = fields.Many2one("hr.employee", "Employee")
     date = fields.Date()
-    week = fields.Integer("Week", compute="_calculate_week", store=False)
+    week = fields.Integer("Week", store=True, compute="_calculate_week")
     time_planned = fields.Integer("Hours Planned", store=True, compute="_calculate_time_planned")
     time_spent = fields.Integer("Hours Spent", store=True, compute="_calculate_time_spent")
-    
+    timesheet = fields.One2many(comodel_name="hr_timesheet.sheet", string="Sheet", compute="_get_timesheet")
+
+    def _get_timesheet(self):
+        self.ensure_one()
+        self.timesheet = self.env["hr_timesheet.sheet"].search([
+                ("date_end", ">=", self.date),
+                ("date_start", "<=", self.date),
+                ("employee_id", "=", self.employee_id.id),
+                # ~ ("company_id", "in", [self.company_id.id, False]),
+                # ~ ("state", "in", ["new", "draft"]),
+            ], limit=1)
+
 
     @api.depends('project_id', 'employee_id', 'date')
     def _calculate_time_planned(self):
@@ -42,6 +52,7 @@ class PlannerCeReport(models.Model):
     def _calculate_time_spent(self):
 
         for rec in self:
+
             beginning_end = rec.get_first_last_day_of_week()
             time_spent_lines = self.env["account.analytic.line"].search([
                                 ('project_id', '=', rec.project_id.id), 
